c1c295c5fca714dc845dbef83ee3e98b9308397b
iosonoira <matteo.dessi93@gmail.com>
2025-11-22 16:48:54 +0100

    Sorry

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,10 +13,9 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa262648d0e380cf0b041a77d180c617eb341f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
-
